abe59e62c4cdff9e49651926b5e19709ab898b2f
Diego Correa <diegocorrea.cc@gmail.com>
2018-02-14 15:24:20 -0300

    inciando model

---
+++ server/models/servers/default_servers_list.py
@@ -0,0 +1,23 @@
+import sqlite3
+import sys
+
+from time import gmtime, strftime
+from config.server import SERVER_DB_PATH
+
+sys.path.append('..')
+
+
+def all():
+    conn = sqlite3.connect(SERVER_DB_PATH)
+    cursor = conn.cursor()
+    cursor.execute("""
+        SELECT * FROM default_servers_list;
+    """)
+    itens = cursor.fetchall()
+    conn.close()
+    if itens is None:
+        return []
+    data = []
+    for linha in itens:
+        data.append(linha)
+    return data
